6476cb9ff6e66443ae6255e707d4a88828129497
sai charan umapathi <sai.charan.umapathi@accenture.com>
2025-12-01 12:34:13 +0530

    just txt

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@
 import './App.css';
 import Header from './Header';
 import ImageSlider from './ImageSlider';
-import UseCallback from './UseCallback';
-import UseEffect from './UseEffect';
-import UseMemo from './UseMemo';
-import UseRef from './UseRef';
+// import UseCallback from './UseCallback';
+// import UseEffect from './UseEffect';
+// import UseMemo from './UseMemo';
+// import UseRef from './UseRef';
 // import ImageSlider from './ImageSlider';
-import Usestate from './Usestate';
+// import Usestate from './Usestate';
 
 function App() {
   return (
     <div className="App flex justify-center flex-col items-center">
-      {/* <Header/> */}
-      {/* <ImageSlider/> */}
+      <Header/>
+      <ImageSlider/>
       {/* <Usestate/> */}
       {/* <UseEffect/> */}
       {/* <UseRef/> */}
       {/* <UseMemo/> */}
-      <UseCallback/>
+      {/* <UseCallback/> */}
     </div>
   );
 }
